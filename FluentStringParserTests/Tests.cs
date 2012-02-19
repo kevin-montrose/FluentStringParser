493c285ee59dd9d3e66e94090dc987bfb4c424f2
Kevin Montrose <kevin.montrose@gmail.com>
2012-02-19 16:53:04 -0500

    Adding more perf tests, have discovered that string values are unacceptably slow; needs further investigation

--- FluentStringParserTests/Tests.cs
+++ FluentStringParserTests/Tests.cs
@@ -92,9 +92,7 @@ namespace FluentStringParserTests
             public Int32? BackEndQueue { get; set; }
             public Int64 Hash { get; set; }
             public Int32? AccountId { get; set; }
-            public Int16? SqlCount { get; set; }
             public Int16? SqlDurationMs { get; set; }
-            public Int16? AspNetDurationMs { get; set; }
             public Int32? ApplicationId { get; set; }
 
             public override string ToString()
@@ -224,9 +222,7 @@ namespace FluentStringParserTests
                         case "IsPageView": row.IsPageView = match.Groups[name].Value == "1"; break;
                         case "RouteName": row.RouteName = match.Groups[name].Value; break;
                         case "AccountId": row.AccountId = int.Parse(match.Groups[name].Value); break;
-                        case "SqlCount": row.SqlCount = short.Parse(match.Groups[name].Value); break;
                         case "SqlDurationMs": row.SqlDurationMs = short.Parse(match.Groups[name].Value); break;
-                        case "AspNetDurationMs": row.AspNetDurationMs = short.Parse(match.Groups[name].Value); break;
                         case "ApplicationId": row.ApplicationId = int.Parse(match.Groups[name].Value); break;
                         case "Method": row.Method = match.Groups[name].Value; break;
                         case "Uri": row.Uri = match.Groups[name].Value; break;
@@ -246,6 +242,176 @@ namespace FluentStringParserTests
             return row;
         }
 
+        private static LogRow ParseWithIndexOf(string _raw)
+        {
+            var row = new LogRow();
+
+            int a = _raw.IndexOf(" ") + 1;
+            int b = _raw.IndexOf(':', a);
+            row.ClientIp = Substring(_raw, a, b);
+
+            a = _raw.IndexOf('[', b) + 1;
+            b = _raw.IndexOf(']', a);
+            var date = Substring(_raw, a, b);
+            row.CreationDate = ParseCreationDate(date);
+
+            a = b + 2;
+            b = _raw.IndexOf(' ', a);
+            row.FrontEnd = Substring(_raw, a, b);
+
+            a = b + 1;
+            b = _raw.IndexOf('/', a);
+            row.BackEnd = Substring(_raw, a, b);
+
+            a = b + 1;
+            b = _raw.IndexOf(' ', a);
+            row.Server = Substring(_raw, a, b);
+
+            a = b + 1;
+            b = _raw.IndexOf('/', a);
+            row.Tq = int.Parse(Substring(_raw, a, b));
+
+            a = b + 1;
+            b = _raw.IndexOf('/', a);
+            row.Tw = int.Parse(Substring(_raw, a, b));
+
+            a = b + 1;
+            b = _raw.IndexOf('/', a);
+            row.Tc = int.Parse(Substring(_raw, a, b));
+
+            a = b + 1;
+            b = _raw.IndexOf('/', a);
+            row.Tr = int.Parse(Substring(_raw, a, b));
+
+            a = b + 1;
+            b = _raw.IndexOf(' ', a);
+            row.Tt = int.Parse(Substring(_raw, a, b));
+
+            a = b + 1;
+            b = _raw.IndexOf(' ', a);
+            row.ResponseCode = short.Parse(Substring(_raw, a, b));
+
+            a = b + 1;
+            b = _raw.IndexOf(' ', a);
+            row.Bytes = int.Parse(Substring(_raw, a, b));
+
+            a = b + 5;
+            b = a + 4;
+            row.TermState = Substring(_raw, a, b);
+
+            a = b + 1;
+            b = _raw.IndexOf('/', a);
+            row.ActConn = int.Parse(Substring(_raw, a, b));
+
+            a = b + 1;
+            b = _raw.IndexOf('/', a);
+            row.FeConn = int.Parse(Substring(_raw, a, b));
+
+            a = b + 1;
+            b = _raw.IndexOf('/', a);
+            row.BeConn = int.Parse(Substring(_raw, a, b));
+
+            a = b + 1;
+            b = _raw.IndexOf('/', a);
+            row.SrvConn = int.Parse(Substring(_raw, a, b));
+
+            a = b + 1;
+            b = _raw.IndexOf(' ', a);
+            row.Retries = int.Parse(Substring(_raw, a, b));
+
+            a = b + 1;
+            b = _raw.IndexOf('/', a);
+            row.SrvQueue = int.Parse(Substring(_raw, a, b));
+
+            a = b + 1;
+            b = _raw.IndexOf(' ', a);
+            row.BackEndQueue = int.Parse(Substring(_raw, a, b));
+
+            a = b + 2;
+            b = _raw.IndexOf('|', a);
+            row.Referer = Substring(_raw, a, b);
+
+            a = b + 1;
+            b = _raw.IndexOf('|', a);
+            row.UserAgent = Substring(_raw, a, b);
+
+            a = b + 1;
+            b = _raw.IndexOf('|', a);
+            row.Host = Substring(_raw, a, b);
+
+            a = b + 1;
+            b = _raw.IndexOf('|', a);
+            row.ForwardFor = Substring(_raw, a, b);
+
+            a = b + 1;
+            b = _raw.IndexOf('}', a);
+            row.AcceptEncoding = Substring(_raw, a, b);
+
+            a = b + 3;
+            b = _raw.IndexOf('|', a);
+            row.ContentEncoding = Substring(_raw, a, b);
+
+            a = b + 1;
+            b = _raw.IndexOf('|', a);
+            var isPageView = Substring(_raw, a, b);
+            if (!string.IsNullOrEmpty(isPageView))
+            {
+                row.IsPageView = byte.Parse(isPageView) == 1;
+            }
+
+            a = b + 1;
+            b = _raw.IndexOf('|', a);
+            var sqlDurationMs = Substring(_raw, a, b);
+            if (!string.IsNullOrEmpty(sqlDurationMs))
+            {
+                row.SqlDurationMs = short.Parse(sqlDurationMs);
+            }
+
+            a = b + 1;
+            b = _raw.IndexOf('|', a);
+            var accountId = Substring(_raw, a, b);
+            if (!string.IsNullOrEmpty(accountId))
+            {
+                row.AccountId = int.Parse(accountId);
+            }
+
+            a = b + 1;
+            b = _raw.IndexOf('}', a);
+            row.RouteName = Substring(_raw, a, b);
+
+            a = b + 3;
+            b = _raw.IndexOf(' ', a);
+            row.Method = Substring(_raw, a, b);
+
+            a = b + 1;
+            b = _raw.IndexOf(' ', a);
+            if (b < 0)
+            {
+                b = _raw.Length;
+            }
+            row.Uri = Substring(_raw, a, b);
+
+            if (b < _raw.Length)
+            {
+                a = b + 1;
+                b = _raw.IndexOf('"', a);
+                row.HttpVersion = Substring(_raw, a, b);
+            }
+
+            return row;
+        }
+
+        public static DateTime ParseCreationDate(string date)
+        {
+            return DateTime.ParseExact(date, "dd/MMM/yyyy:HH:mm:ss.fff", CultureInfo.InvariantCulture, DateTimeStyles.None);
+        }
+
+        private static string Substring(string _raw, int a, int b)
+        {
+            var result = _raw.Substring(a, b - a);
+            return result;
+        }
+
         [TestMethod]
         public void HaProxyLogs()
         {
@@ -257,8 +423,10 @@ namespace FluentStringParserTests
                 {
                     var row1 = ParseWithRegex(line);
                     var row2 = ParseWithFTemplate(line);
+                    var row3 = ParseWithIndexOf(line);
 
                     Assert.AreEqual(row1.ToString(), row2.ToString(), "For: " + line);
+                    Assert.AreEqual(row2.ToString(), row3.ToString(), "For: " + line);
                 }
             }
         }
@@ -308,15 +476,61 @@ namespace FluentStringParserTests
                     template.Stop();
                 };
 
-            // Run both orders
+            var indexOf = new Stopwatch();
+            Action runIndexOf =
+                delegate
+                {
+                    // warmup
+                    foreach (var line in lines) ParseWithIndexOf(line);
+
+                    indexOf.Restart();
+                    for (int i = 0; i < 1000; i++)
+                    {
+                        for (int j = 0; j < lines.Length; j++)
+                        {
+                            ParseWithIndexOf(lines[j]);
+                        }
+                    }
+                    indexOf.Stop();
+                };
+
+            // Run in all orders
 
             runRegex();
             runTemplate();
+            runIndexOf();
             Assert.IsTrue(template.ElapsedMilliseconds < regex.ElapsedMilliseconds);
+            Assert.IsTrue(template.ElapsedMilliseconds < indexOf.ElapsedMilliseconds);
+
+            runRegex();
+            runIndexOf();
+            runTemplate();
+            Assert.IsTrue(template.ElapsedMilliseconds < regex.ElapsedMilliseconds);
+            Assert.IsTrue(template.ElapsedMilliseconds < indexOf.ElapsedMilliseconds);
 
             runTemplate();
             runRegex();
+            runIndexOf();
             Assert.IsTrue(template.ElapsedMilliseconds < regex.ElapsedMilliseconds);
+            Assert.IsTrue(template.ElapsedMilliseconds < indexOf.ElapsedMilliseconds);
+
+            runTemplate();
+            runIndexOf();
+            runRegex();
+            Assert.IsTrue(template.ElapsedMilliseconds < regex.ElapsedMilliseconds);
+            Assert.IsTrue(template.ElapsedMilliseconds < indexOf.ElapsedMilliseconds);
+
+            runIndexOf();
+            runTemplate();
+            runRegex();
+            Assert.IsTrue(template.ElapsedMilliseconds < regex.ElapsedMilliseconds);
+            Assert.IsTrue(template.ElapsedMilliseconds < indexOf.ElapsedMilliseconds);
+
+            runIndexOf();
+            runRegex();
+            runTemplate();
+            Assert.IsTrue(template.ElapsedMilliseconds < regex.ElapsedMilliseconds);
+            Assert.IsTrue(template.ElapsedMilliseconds < indexOf.ElapsedMilliseconds);
         }
 
         class DecimalObject
@@ -739,7 +953,7 @@ namespace FluentStringParserTests
             Assert.AreEqual("world", obj.Raw2);
         }
 
-        class PerfObject
+        class IntPerfObject
         {
             public int A;
             public int B;
@@ -748,12 +962,12 @@ namespace FluentStringParserTests
         }
 
         [TestMethod]
-        public void SimplePerf()
+        public void SimpleIntPerf()
         {
             var regex = new Regex(@"(\d+),(\d+),(\d+),(\d+)", RegexOptions.Compiled);
             var parserTask =
                 FSBuilder
-                    .Take<PerfObject>(",", "A")
+                    .Take<IntPerfObject>(",", "A")
                     .Take(",", "B")
                     .Take(",", "C")
                     .TakeRest("D")
@@ -761,8 +975,8 @@ namespace FluentStringParserTests
 
             Action ghettoGC = () => { GC.Collect(); Thread.Sleep(100); };
 
-            Action<string, PerfObject> regexTask =
-                delegate(string str, PerfObject obj)
+            Action<string, IntPerfObject> regexTask =
+                delegate(string str, IntPerfObject obj)
                 {
                     var matches = regex.Match(str);
 
@@ -772,8 +986,8 @@ namespace FluentStringParserTests
                     obj.D = int.Parse(matches.Groups[4].Value);
                 };
 
-            Action<string, PerfObject> handTask =
-                delegate(string str, PerfObject obj)
+            Action<string, IntPerfObject> handTask =
+                delegate(string str, IntPerfObject obj)
                 {
                     int a = str.IndexOf(',');
                     obj.A = int.Parse(str.Substring(0, a));
@@ -797,9 +1011,9 @@ namespace FluentStringParserTests
             // Equivalence check
             foreach (var str in data)
             {
-                var o1 = new PerfObject();
-                var o2 = new PerfObject();
-                var o3 = new PerfObject();
+                var o1 = new IntPerfObject();
+                var o2 = new IntPerfObject();
+                var o3 = new IntPerfObject();
 
                 parserTask(str, o1);
                 regexTask(str, o2);
@@ -817,7 +1031,7 @@ namespace FluentStringParserTests
             results[2] = new List<long>();   // parser
 
             var timer = new Stopwatch();
-            var speedObj = new PerfObject();
+            var speedObj = new IntPerfObject();
 
             for (int i = 0; i < 5; i++)
             {
@@ -859,6 +1073,128 @@ namespace FluentStringParserTests
 
             Assert.IsTrue(medianRegex > medianHand);
             Assert.IsTrue(medianHand > medianParser);
+        }
+
+        class StringPerfObject
+        {
+            public string A { get; set; }
+            public string B { get; set; }
+            public string C { get; set; }
+            public string D { get; set; }
+        }
+
+        [TestMethod]
+        public void SimpleStringPerf()
+        {
+            var regex = new Regex(@"([^,]*?),([^,]*?),([^,]*?),([^,]*)", RegexOptions.Compiled);
+            var parserTask =
+                FSBuilder
+                    .Take<StringPerfObject>(",", "A")
+                    .Take(",", "B")
+                    .Take(",", "C")
+                    .TakeRest("D")
+                    .Seal();
+
+            Action ghettoGC = () => { GC.Collect(); Thread.Sleep(100); };
+
+            Action<string, StringPerfObject> regexTask =
+                delegate(string str, StringPerfObject obj)
+                {
+                    var matches = regex.Match(str);
+
+                    obj.A = matches.Groups[1].Value;
+                    obj.B = matches.Groups[2].Value;
+                    obj.C = matches.Groups[3].Value;
+                    obj.D = matches.Groups[4].Value;
+                };
+
+            Action<string, StringPerfObject> handTask =
+                delegate(string str, StringPerfObject obj)
+                {
+                    int a = str.IndexOf(',');
+                    obj.A = str.Substring(0, a);
+
+                    int b = str.IndexOf(',', a + 1);
+                    obj.B = str.Substring(a + 1, b - (a + 1));
+
+                    int c = str.IndexOf(',', b + 1);
+                    obj.C = str.Substring(b + 1, c - (b + 1));
+
+                    obj.D = str.Substring(c + 1, str.Length - (c + 1));
+                };
+
+            var rand = new Random();
+            var data = new List<string>();
+            for (int i = 0; i < 1000000; i++)
+            {
+                data.Add(Guid.NewGuid() + "," + Guid.NewGuid() + "," + Guid.NewGuid() + "," + Guid.NewGuid());
+            }
+
+            // Equivalence check
+            foreach (var str in data)
+            {
+                var o1 = new StringPerfObject();
+                var o2 = new StringPerfObject();
+                var o3 = new StringPerfObject();
+
+                parserTask(str, o1);
+                regexTask(str, o2);
+                handTask(str, o3);
+
+                if (o1.A != o2.A || o2.A != o3.A) throw new Exception();
+                if (o1.B != o2.B || o2.B != o3.B) throw new Exception();
+                if (o1.C != o2.C || o2.C != o3.C) throw new Exception();
+                if (o1.D != o2.D || o2.D != o3.D) throw new Exception();
+            }
+
+            var results = new Dictionary<int, List<long>>();
+            results[0] = new List<long>();   // regex
+            results[1] = new List<long>();   // hand
+            results[2] = new List<long>();   // parser
+
+            var timer = new Stopwatch();
+            var speedObj = new StringPerfObject();
+
+            for (int i = 0; i < 5; i++)
+            {
+                ghettoGC();
+
+                timer.Restart();
+                foreach (var str in data)
+                {
+                    regexTask(str, speedObj);
+                }
+                timer.Stop();
+
+                results[0].Add(timer.ElapsedMilliseconds);
+                ghettoGC();
+
+                timer.Restart();
+                foreach (var str in data)
+                {
+                    handTask(str, speedObj);
+                }
+                timer.Stop();
+
+                results[1].Add(timer.ElapsedMilliseconds);
+                ghettoGC();
+
+                timer.Restart();
+                foreach (var str in data)
+                {
+                    parserTask(str, speedObj);
+                }
+                timer.Stop();
+
+                results[2].Add(timer.ElapsedMilliseconds);
+            }
+
+            var medianRegex = results[0].OrderBy(o => o).ElementAt(results[0].Count / 2);
+            var medianHand = results[1].OrderBy(o => o).ElementAt(results[1].Count / 2);
+            var medianParser = results[2].OrderBy(o => o).ElementAt(results[2].Count / 2);
+
+            Assert.IsTrue(medianRegex > medianHand, "Regex faster than hand rolled; invalid test");
+            Assert.IsTrue(medianHand > medianParser, "Hand faster than generated; bad parser");
         }
     }
 }
